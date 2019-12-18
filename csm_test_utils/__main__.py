5e0bf5c57022a9a85bfb66894a30e8af1e614538
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2019-12-18 10:34:51 +0300

    removed empty string

--- csm_test_utils/__main__.py
+++ csm_test_utils/__main__.py
@@ -1,9 +1,8 @@
+from csm_test_utils.autoscaling import main as as_main
 from csm_test_utils.common import root_parser
 from csm_test_utils.continuous import main as c_main
 from csm_test_utils.continuous_entities import main as rds_main
 from csm_test_utils.rebalance_test import main as r_main
-from csm_test_utils.autoscaling import main as as_main
-
 
 args = root_parser.parse_args()
 if args.test == "monitor":
