7fb71fec6022d1930e944238f7fcca729ab55eff
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2020-02-13 17:13:36 +0300

    ape's idea fix

--- csm_test_utils/as_loadbalancer.py
+++ csm_test_utils/as_loadbalancer.py
@@ -6,27 +6,28 @@ import time
 import requests
 from ocomone.logging import setup_logger
 
-from .common import Client, base_parser, sub_parsers
+from .common import base_parser, sub_parsers
 
-telegraf = "localhost:8080/telegraf"
+tgf_address = "localhost:8080/telegraf"
 MEASUREMENT = "as_loadbalancer"
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
 
 
-def get(client: Client):
+def report(target):
     """Send request and write metrics to telegraf"""
-    if requests.get("http://localhost:8080/").status_code == 200:
+    if requests.get(target).status_code == 200:
         result = "connected"
         reason = "ok"
-        LOGGER.info('OK ADD')
+        LOGGER.info("OK")
     else:
         result = "connection_lost"
         reason = "fail"
-        LOGGER.info('FAIL ADD')
+        LOGGER.info("FAIL")
 
     influx_row = f"${MEASUREMENT},reason=${reason} state=\"${result}\""
-    client.report_metric(influx_row)
+    res = requests.post(tgf_address, data=influx_row)
+    assert res.status_code == 204, f"Status is {res.status_code}"
 
 
 AGP = sub_parsers.add_parser("as_load", add_help=False, parents=[base_parser])
@@ -36,11 +37,10 @@ def main():
     """Start monitoring autoscaling loadbalancer"""
     args, _ = AGP.parse_known_args()
     setup_logger(LOGGER, "continuous", log_dir=args.log_dir, log_format="[%(asctime)s] %(message)s")
-    client = Client(args.target, args.telegraf)
-    LOGGER.info(f"Started monitoring of {client.url} (telegraf at {client.tgf_address})")
+    LOGGER.info(f"Started monitoring of {args.target} (telegraf at {args.telegraf})")
     while True:
         try:
-            get(client)
+            report(args.target)
             time.sleep(10)
         except KeyboardInterrupt:
             LOGGER.info("Monitoring Stopped")
