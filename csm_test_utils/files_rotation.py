4c575158e54db6e69b83f9a92ac4f0939fda8dca
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-12-28 15:04:30 +0300

    clearing folder via subprocess

--- csm_test_utils/files_rotation.py
+++ csm_test_utils/files_rotation.py
@@ -6,6 +6,7 @@ import time
 import hashlib
 import os
 import requests
+import subprocess
 from influx_line_protocol import Metric, MetricCollection
 from ocomone.logging import setup_logger
 
@@ -43,10 +44,9 @@ def md5(file_name):
 def create_file(dd_input="/dev/urandom", base_file="/tmp/base_file.data", bs=1200000, count=100):
     base_copy = f"{base_file}_copy"
     if not os.path.exists(base_file) or (round(time.time() - os.path.getmtime(base_file)) / 60) > 60:
-        try:
-            os.system(f"rm {base_file.split('.')[0]}*")
-        except Exception as Ex:
-            LOGGER.error(Ex)
+        files = [i for i in os.listdir()]
+        if files:
+            subprocess.call(['rm', '-r'] + files)
         os.system(f"/bin/dd if={dd_input} of={base_file} bs={bs} count={count}")
         LOGGER.info(f"Base file created at {base_file}")
         base_hash = md5(base_file)
