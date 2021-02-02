47e2b6c89c2745e5014b56dae0aeb829c6454d19
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2021-02-02 12:48:31 +0300

    new lb_load test for graphite reports

--- csm_test_utils/loadbalancer/lb_monitor.py
+++ csm_test_utils/loadbalancer/lb_monitor.py
@@ -1,9 +1,44 @@
+import logging
+import socket
+
+import requests
+from ocomone.logging import setup_logger
+
 from ..common import base_parser, sub_parsers
+
+LB_TIMING = "lb_timing"
+LB_TIMEOUT = "lb_timeout"
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
 
 AGP = sub_parsers.add_parser("lb_load", add_help=False, parents=[base_parser])
 
+
 def main():
-    print("1")
+    args, _ = AGP.parse_known_args()
+    setup_logger(LOGGER, "lb_load", log_dir=args.log_dir, log_format="[%(asctime)s] %(message)s")
+    timeout = 20
+    try:
+        res = requests.get(args.target, headers={"Connection": "close"}, timeout=timeout)
+    except Exception as Ex:
+        LOGGER.exception("Timeout sending request to LB")
+        result = {
+            "reason": LB_TIMEOUT,
+            "client": socket.gethostname(),
+            "timeout": timeout * 1000,
+            "exception": Ex
+        }
+    else:
+        result = {
+            "reason": LB_TIMING,
+            "client": socket.gethostname(),
+            "server": res.headers["Server"],
+            "elapsed": res.elapsed.microseconds / 1000
+        }
+
+    print(result)
+    exit(0)
 
 
 if __name__ == "__main__":
