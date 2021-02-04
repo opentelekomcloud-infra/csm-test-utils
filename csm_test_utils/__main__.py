fd21df8826f2fd7b3cb5da01c06d238d8c9f33ad
Anton Kachurin <anton.kachurin@t-systems.com>
2021-02-04 10:25:15 +0300

    Small changes

--- csm_test_utils/__main__.py
+++ csm_test_utils/__main__.py
@@ -4,18 +4,30 @@ from inspect import signature
 from csm_test_utils.parsers import root_parser
 
 ENTRY_POINTS = {
-    "monitor": "csm_test_utils.continuous",
-    "rebalance": "csm_test_utils.rebalance_test",
-    "rds_monitor": "csm_test_utils.continuous_entities",
-    "as_monitor": "csm_test_utils.autoscaling.smn_webhook",
-    "as_load": "csm_test_utils.autoscaling.loadbalancer",
-    "sfs_compare": "csm_test_utils.files_rotation",
-    "internal_dns_resolve": "csm_test_utils.dns.dns_resolving",
-    "internal_dns_host_check": "csm_test_utils.dns.host_check",
-    "rds_backup_monitor": "csm_test_utils.rds.rds_backup",
-    "lb_load": "csm_test_utils.loadbalancer.lb_monitor",
-    "rds_backup_generate_data": "csm_test_utils.rds_backup.generation.cli",
-    "rds_backup_check": "csm_test_utils.rds_backup.backup_check.rds_backup",
+    "monitor":
+        "csm_test_utils.continuous",
+    "rebalance":
+        "csm_test_utils.rebalance_test",
+    "rds_monitor":
+        "csm_test_utils.continuous_entities",
+    "as_monitor":
+        "csm_test_utils.autoscaling.smn_webhook",
+    "as_load":
+        "csm_test_utils.autoscaling.loadbalancer",
+    "sfs_compare":
+        "csm_test_utils.files_rotation",
+    "internal_dns_resolve":
+        "csm_test_utils.dns.dns_resolving",
+    "internal_dns_host_check":
+        "csm_test_utils.dns.host_check",
+    "rds_backup_monitor":
+        "csm_test_utils.rds.rds_backup",
+    "lb_load":
+        "csm_test_utils.loadbalancer.lb_monitor",
+    "rds_backup_generate_data":
+        "csm_test_utils.rds_backup.generation.cli",
+    "rds_backup_check":
+        "csm_test_utils.rds_backup.backup_check.rds_backup",
 }
 
 
@@ -26,7 +38,6 @@ def _check_main(_main):
 
 def main(args=None):
     """Main csm_test_utils entry point"""
-
     args, _ = root_parser.parse_known_args(args=args)
     import_path = ENTRY_POINTS[args.test]
     module = import_module(import_path)
